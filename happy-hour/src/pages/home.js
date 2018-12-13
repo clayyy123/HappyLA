8dc5ada8c364661c11862d8f1c09ebee54d8046c
Clayton Wang <claydwang@gmail.com>
2018-12-13 00:42:33 -0800

    home page

--- happy-hour/src/pages/home.js
+++ happy-hour/src/pages/home.js
@@ -3,12 +3,16 @@ import React, { Component } from 'react';
 class Home extends Component {
   render() {
     return (
+      // <div className="home-wrapper">
       <div className="home">
-        <h1 className="home__title">Binge</h1>
-        <h2 className="home__slogan">Happy Hour Finder</h2>
-        <input className="home__location" placeholder="Enter Location" />
-        <button className="home__button">Search</button>
+        <h1 className="home__title">BINGE</h1>
+        <h2 className="home__slogan">FIND YOUR NEXT BAR</h2>
+        <div className="home__search">
+          <input className="home__location" placeholder="Enter Location" />
+          <button className="home__button">Start Binging!</button>
+        </div>
       </div>
+      // </div>
     );
   }
 }
