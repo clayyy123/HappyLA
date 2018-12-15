6c886ce7b8f314fd251294ffbffe9455d3da1036
Clayton Wang <claydwang@gmail.com>
2018-12-14 20:44:48 -0800

    card

--- happy-hour/src/pages/feed.js
+++ happy-hour/src/pages/feed.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
 
 class Feed extends Component {
+  state = {
+    restaraunt: {
+      name: 'Boulevard574',
+      add: '574 N. DIAMOND BAR BLVD.',
+      mon: '4-7',
+      tue: '4-7',
+      wed: '4-7',
+      thursday: '4-7',
+      friday: '4-7',
+      everyday: [true, '4-7'],
+      food: true,
+      alk: true
+    }
+  };
   render() {
-    return <h1>hi</h1>;
+    return (
+      <div className="card">
+        <h1 className="card__title">{this.state.restaurant.name}</h1>
+      </div>
+    );
   }
 }
 
