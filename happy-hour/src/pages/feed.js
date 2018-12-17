af9437f38652ee5578fd3e49124629155217ddb4
Clayton Wang <claydwang@gmail.com>
2018-12-17 13:38:40 -0800

    lights and apikey

--- happy-hour/src/pages/feed.js
+++ happy-hour/src/pages/feed.js
@@ -2,23 +2,87 @@ import React, { Component } from 'react';
 
 class Feed extends Component {
   state = {
+    days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
     restaurant: {
       name: 'Boulevard574',
       add: '574 N. DIAMOND BAR BLVD.',
-      mon: '4-7',
-      tue: '4-7',
-      wed: '4-7',
-      thursday: '4-7',
-      friday: '4-7',
-      everyday: [true, '4-7'],
+      0: '',
+      1: [4, 5, 6, 7],
+      2: [4, 5, 6, 7],
+      3: [4, 5, 6, 7],
+      4: [4, 5, 6, 7],
+      5: [4, 5, 6, 7],
+      6: '',
+      everyday: [true, [4, 5, 6, 7]],
       food: true,
       alk: true
-    }
+    },
+    date: new Date()
   };
+
+  currentDay() {
+    return this.state.date.getDay();
+  }
+
+  currentName() {
+    return this.state.days[this.currentDay()];
+  }
+
+  lightHandler() {
+    let hour = this.state.date.getHours();
+    let end = this.state.restaurant[this.currentDay()][
+      this.state.restaurant[this.currentDay()].length - 1
+    ];
+    console.log(this.state.restaurant[this.currentDay()].indexOf(hour));
+    if (hour > 12) {
+      hour = hour - 12;
+      console.log('what');
+    }
+    if (this.state.restaurant[this.currentDay()].indexOf(hour) !== -1) {
+      if (end - hour <= 1) {
+        return (
+          <div className="card__light">
+            <div className="card__dot" />
+            <div className="card__dot card__yellow" />
+            <div className="card__dot" />
+          </div>
+        );
+      } else if (end - hour > 1) {
+        return (
+          <div className="card__light">
+            <div className="card__dot" />
+            <div className="card__dot" />
+            <div className="card__dot card__green" />
+          </div>
+        );
+      }
+    } else {
+      return (
+        <div className="card__light">
+          <div className="card__dot card__red" />
+          <div className="card__dot" />
+          <div className="card__dot" />
+        </div>
+      );
+    }
+  }
+
   render() {
     return (
       <div className="card">
-        <h1 className="card__title">{this.state.restaurant.name}</h1>
+        <div className="card__info">
+          <h1 className="card__title">{this.state.restaurant.name}</h1>
+          <h2>
+            Today: {this.state.restaurant[this.currentDay()][0]}pm -
+            {
+              this.state.restaurant[this.currentDay()][
+                this.state.restaurant[this.currentDay()].length - 1
+              ]
+            }
+            pm
+          </h2>
+        </div>
+        {this.lightHandler()}
       </div>
     );
   }
